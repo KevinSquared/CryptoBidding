7b33c890002e97c59908ae520cd42ee160955c53
Kevin Zou <kevin.zp.zou@gmail.com>
2022-03-11 16:06:34 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react'
+import CanvasDraw from 'react-canvas-draw'
 import './App.css';
 
 function App() {
+  let canvasRef = useRef()
+  const [save, setSave] = useState()
+
+  useEffect(() => {
+    console.log(save)
+  }, [save])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => canvasRef.eraseAll()}>
+          Erase
+        </button>
+        <button onClick={() => canvasRef.undo()}>
+          Undo
+        </button>
+        <button onClick={() => setSave(canvasRef.getSaveData())}>
+          Save
+        </button>
+        <button onClick={() => canvasRef.loadSaveData(save)}>
+          Load
+        </button>
+        <CanvasDraw
+          ref={canvasDraw => (canvasRef = canvasDraw)}
+          canvasWidth={400}
+          canvasHeight={600}
+          lazyRadius={0}
+          brushRadius={4}
+          hideGrid
+        />
       </header>
     </div>
   );
